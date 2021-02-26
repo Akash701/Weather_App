6242da75da11d5c60fc1d52ee8f79df8dcda1a76
Akash J Nair <akashjnair701@gmail.com>
2021-02-26 21:34:39 +0530

    Clima Completed

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -15,7 +15,7 @@ class _CityScreenState extends State<CityScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/city_background.jpg'),
+            image: AssetImage('images/city.jpg'),
             fit: BoxFit.cover,
           ),
         ),
@@ -31,17 +31,34 @@ class _CityScreenState extends State<CityScreen> {
                   },
                   child: Icon(
                     Icons.arrow_back_sharp,
-                    size: 50.0,
+                    size: 30.0,
                   ),
                 ),
               ),
               Container(
-                padding: EdgeInsets.all(20.0),
+                padding: EdgeInsets.all(40.0),
                 child: TextField(
                   style: TextStyle(
                     color: Colors.black,
                   ),
-                  decoration: kTextfieldProperty,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    icon: Icon(
+                      Icons.location_city,
+                      color: Colors.white,
+                    ),
+                    hintText: ('Enter city name'),
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
                   onChanged: (value) {
                     cityName = value;
                   },
